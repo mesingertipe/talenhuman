d61e48bf02f235bcbfe24d82be111e46b99e26e1
tpedraza <tpedraza@tyd.com.co>
2026-04-17 22:40:57 -0500

    Fix: Synchronize DB schema for Special Dates intelligence

---
+++ src/TalenHuman.Infrastructure/Persistence/Migrations/20260418033925_AddSpecialDateIntelligenceFields.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TalenHuman.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSpecialDateIntelligenceFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CountryCode",
+                table: "PredictiveSpecialDates",
+                newName: "Country");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSystem",
+                table: "PredictiveSpecialDates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSystem",
+                table: "PredictiveSpecialDates");
+
+            migrationBuilder.RenameColumn(
+                name: "Country",
+                table: "PredictiveSpecialDates",
+                newName: "CountryCode");
+        }
+    }
+}
