f9471fb579159cae6c66d6a76231e81b7463b002
tpedraza <tpedraza@tyd.com.co>
2026-05-12 21:39:25 -0500

    UI: Revert start_url to root to stabilize sessions

--- src/TalenHuman.Web/vite.config.js
+++ src/TalenHuman.Web/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
         display: 'standalone',
         orientation: 'portrait',
         scope: '/',
-        start_url: '/?v=46',
+        start_url: '/',
         icons: [
           {
             src: 'icon-192-v21.png',
